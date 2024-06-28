e74958db6420032b098603d2a020b7abf17afa93
neil <eon.neil@gmail.com>
2024-06-28 11:23:07 +0530

    org-control

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -4,10 +4,9 @@ const OrganizationLayout = ({
     children: React.ReactNode;
 }) => {
     return(
-        <div className="h-full">
-            <Navbar />
+        <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
             {children}
-        </div>
+        </main>
     );
 };
 
