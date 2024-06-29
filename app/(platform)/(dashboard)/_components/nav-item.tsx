28e2f0801c8f0dde5044ac29099461a10a3ea765
neil <eon.neil@gmail.com>
2024-06-29 11:52:15 +0530

    mobile navbar

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -81,7 +81,12 @@ const onClick = (href: string) => {
                         className="rounded-sm object-cover"
                         />
 
+                       
+
                     </div>
+                    <span className="font-medium text-sm">
+                            {organization.name}
+                        </span>
                     </div>
             </AccordionTrigger>
             <AccordionContent className="pt-1 text-neutral-700">
