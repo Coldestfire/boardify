33338ef7708ec866aa6df7b36b3888e510b315ec
neil <eon.neil@gmail.com>
2024-06-29 12:09:32 +0530

    skeleton

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -10,6 +10,7 @@ import {
  Settings
 } from "lucide-react"
 import { Button } from "@/components/ui/button";
+import { Skeleton } from '@/components/ui/skeleton';
 
 
 export type Organization = {
@@ -109,3 +110,14 @@ const onClick = (href: string) => {
         </AccordionItem>
     )
 }
+
+NavItem.Skeleton = function SkeletonNavItem() {
+    return(
+        <div className="flex items-center gap-x-2">
+            <div className="w-10 h-10 relative shrink-0">
+            <Skeleton className="h-full w-full absolute" />
+            </div>
+            <Skeleton className="h-10 w-full" />
+        </div>
+    );
+};
